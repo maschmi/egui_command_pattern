fe044b2bab541c2aed1fe617c092fee3c6dcbcac
maschmi <maschmi@maschmi.net>
2024-09-25 17:10:34 +0200

    initial commit
    setup steps for egui eframe template

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './egui_command_pattern.js',
+  './egui_command_pattern_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
